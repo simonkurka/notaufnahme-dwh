3eeeaa84a3493b6b03f46383b464207a0d925af4
skurka <skurka@ukaachen.de>
2021-07-15 15:19:49 +0200

    fix bad permissions

--- debian/build.sh
+++ debian/build.sh
@@ -14,7 +14,7 @@ sed -e "s/__PACKAGE__/$PACKAGE/g" -e "s/__VERSION__/$VERSION/g" $DIR/control > $
 cp $DIR/preinst $DBUILD/DEBIAN/
 cp $DIR/postinst $DBUILD/DEBIAN/
 cp $DIR/prerm $DBUILD/DEBIAN/
-sed -e "/^__AKTIN_DROP__/{r $DRESOURCES/database/aktin_postgres_drop.sql" -e 'd;}' $DIR/postrm > $DBUILD/DEBIAN/postrm
+sed -e "/^__AKTIN_DROP__/{r $DRESOURCES/database/aktin_postgres_drop.sql" -e 'd;}' $DIR/postrm > $DBUILD/DEBIAN/postrm && chmod 0755 $DBUILD/DEBIAN/postrm
 
 #
 # Changelog
