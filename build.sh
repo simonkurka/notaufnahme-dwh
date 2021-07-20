9fd50501abde493dd45aef64dfa7a162cb88b097
skurka <skurka@ukaachen.de>
2021-07-20 13:28:24 +0200

    common build.sh split into sections

--- build.sh
+++ build.sh
@@ -1,23 +1,74 @@
 #!/bin/bash
 set -e
 
-PACKAGE=$1
-VERSION=$2
-DBUILD=$3
+# Check if variables are empty
+if [ -z "${PACKAGE}" ]; then echo "\$PACKAGE is empty."; exit 1; fi
+if [ -z "${VERSION}" ]; then echo "\$VERSION is empty."; exit 1; fi
+if [ -z "${DBUILD}" ]; then echo "\$DBUILD is empty."; exit 1; fi
 
 # Directory this script is located in + /resources
 DRESOURCES="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )/resources"
 
-mkdir -p $DBUILD/opt/wildfly/standalone/deployments/
-wget -P $DBUILD/opt/wildfly/standalone/deployments https://www.aktin.org/software/repo/org/aktin/dwh/dwh-j2ee/$VERSION/dwh-j2ee-$VERSION.ear
+set -a
+. "${DRESOURCES}/versions"
+set +a
 
-mkdir -p $DBUILD/opt/wildfly/standalone/configuration/
-mkdir -p $DBUILD/etc/aktin
-cp $DRESOURCES/aktin.properties $DBUILD/etc/aktin/
+function dwh_j2ee() {
+	DWILDFLYDEPLOYMENTS="${1}"
 
-#
-# Post-install resources
-#
-mkdir -p $DBUILD/usr/share/$PACKAGE
-cp -r $DRESOURCES/database $DRESOURCES/datasource $DBUILD/usr/share/$PACKAGE/
+	mkdir -p "${DBUILD}${DWILDFLYDEPLOYMENTS}"
+	mvn dependency:get -DremoteRepositories="https://www.aktin.org/software/repo/" -Dartifact="org.aktin.dwh:dwh-j2ee:${VDWH_J2EE}:ear"
+	# dirty
+	cp ~/".m2/repository/org/aktin/dwh/dwh-j2ee/${VDWH_J2EE}/dwh-j2ee-${VDWH_J2EE}.ear" "${DBUILD}${DWILDFLYDEPLOYMENTS}/dwh-j2ee-${VDWH_J2EE}.ear"
+}
+
+function aktin_properties() {
+	DAKTINCONF="${1}"
+
+	mkdir -p "${DBUILD}${DAKTINCONF}"
+	cp "${DRESOURCES}/aktin.properties" "${DBUILD}${DAKTINCONF}/"
+}
+
+function aktin_dir() {
+	DAKTIN="${1}"
+
+	mkdir -p "${DBUILD}${DAKTIN}"
+}
+
+function aktin_importscripts() {
+	DAKTINIMPORTSCRIPTS="${1}"
+
+	mkdir -p "$(dirname "${DBUILD}${DAKTINIMPORTSCRIPTS}")"
+	cp -r "${DRESOURCES}/import-scripts" "${DBUILD}${DAKTINIMPORTSCRIPTS}"
+}
+
+function aktin_importdir() {
+	DAKTINIMPORT="${1}"
+
+	mkdir -p "${DBUILD}${DAKTINIMPORT}"
+}
+
+function database_postinstall() {
+	DDBPOSTINSTALL="$1"
+
+	mkdir -p "$(dirname "${DBUILD}${DDBPOSTINSTALL}")"
+	cp -r "${DRESOURCES}/database" "${DBUILD}${DDBPOSTINSTALL}"
+}
+
+function datasource_postinstall() {
+	DDSPOSTINSTALL="$1"
+
+	mkdir -p "$(dirname "${DBUILD}${DDSPOSTINSTALL}")"
+	cp -r "${DRESOURCES}/datasource" "${DBUILD}${DDSPOSTINSTALL}"
+}
+
+function build_linux() {
+	dwh_j2ee "/opt/wildfly/standalone/deployments"
+	aktin_properties "/etc/aktin"
+	aktin_dir "/var/lib/aktin"
+	aktin_importscripts "/var/lib/aktin/import-scripts"
+	aktin_importdir "/var/lib/aktin/import"
+	database_postinstall "/usr/share/${PACKAGE}/database"
+	datasource_postinstall "/usr/share/${PACKAGE}/datasource"
+}
 
--- debian/build.sh
+++ debian/build.sh
@@ -1,28 +1,39 @@
 #!/bin/bash
 set -e
-PACKAGE=$1
-VERSION=$2
+
+# Required parameters
+PACKAGE="${1}"
+VERSION="${2}"
+
+# Check if variables are empty
+if [ -z "${PACKAGE}" ]; then echo "\$PACKAGE is empty."; exit 1; fi
+if [ -z "${VERSION}" ]; then echo "\$VERSION is empty."; exit 1; fi
 
 # Directory this script is located in
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-DBUILD="$DIR/build/${PACKAGE}_${VERSION}"
-DEPEND_I2B2="$(echo "$PACKAGE" | awk -F '-' '{print $1"-"$2"-i2b2"}')"
+DBUILD="${DIR}/build/${PACKAGE}_${VERSION}"
+DEPEND_I2B2="$(echo "${PACKAGE}" | awk -F '-' '{print $1"-"$2"-i2b2"}')"
+
+# Cleanup
+rm -rf "${DIR}/build"
 
 # Load common linux files
-source $(dirname "$DIR")/build.sh "$PACKAGE" "$VERSION" "$DBUILD"
+. "$(dirname "${DIR}")/build.sh"
+build_linux
 
-mkdir -p $DBUILD/DEBIAN
-sed -e "s/__PACKAGE__/$PACKAGE/g" \
-    -e "s/__VERSION__/$VERSION/g" \
-    -e "s/__DEPEND_I2B2__/$DEPEND_I2B2/g" \
-    $DIR/control > $DBUILD/DEBIAN/control
-sed -e "s/__I2B2_SHARED__/$(echo $PACKAGE | awk -F '-' '{print $1"-"$2}')/g" \
-    -e "s/__PACKAGE__/$PACKAGE/g" \
-    $DIR/templates > $DBUILD/DEBIAN/templates
-cp $DIR/preinst $DBUILD/DEBIAN/
-cp $DIR/postinst $DBUILD/DEBIAN/
-cp $DIR/prerm $DBUILD/DEBIAN/
-sed -e "/^__AKTIN_DROP__/{r $DRESOURCES/database/aktin_postgres_drop.sql" -e 'd;}' $DIR/postrm > $DBUILD/DEBIAN/postrm && chmod 0755 $DBUILD/DEBIAN/postrm
+mkdir -p "${DBUILD}/DEBIAN"
+sed -e "s/__PACKAGE__/${PACKAGE}/g" \
+    -e "s/__VERSION__/${VERSION}/g" \
+    -e "s/__DEPEND_I2B2__/${DEPEND_I2B2}/g" \
+    "${DIR}/control" > "${DBUILD}/DEBIAN/control"
+sed -e "s/__I2B2_SHARED__/$(echo "${PACKAGE}" | awk -F '-' '{print $1"-"$2}')/g" \
+    -e "s/__PACKAGE__/${PACKAGE}/g" \
+    "${DIR}/templates" > "${DBUILD}/DEBIAN/templates"
+cp "${DIR}/preinst" "${DBUILD}/DEBIAN/"
+cp "${DIR}/postinst" "${DBUILD}/DEBIAN/"
+cp "${DIR}/prerm" "${DBUILD}/DEBIAN/"
+sed -e "/^__AKTIN_DROP__/{r ${DRESOURCES}/database/aktin_postgres_drop.sql" -e 'd;}' "${DIR}/postrm" > "${DBUILD}/DEBIAN/postrm" && chmod 0755 "${DBUILD}/DEBIAN/postrm"
 
-dpkg-deb --build $DBUILD
+dpkg-deb --build "${DBUILD}"
+rm -rf "${DBUILD}"
 
